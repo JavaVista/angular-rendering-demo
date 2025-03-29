592f1416f561bc03e71a876f02daa4fe606b6afc
Javi Carrion <suavejavi@gmail.com>
2025-03-28 23:33:05 -0400

    refactor: update layout and add two-way binding demo with signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OuterCounterComponent } from './CoarseRenderingComponents/outer-counter/outer-counter.component';
 import { OuterCounterSignalComponent } from './FineGrainedRenderingComponents/outer-counter-signal/outer-counter-signal.component';
@@ -11,4 +11,14 @@ import { OuterCounterSignalComponent } from './FineGrainedRenderingComponents/ou
 })
 export class AppComponent {
   title = 'Ng Rendering Demo';
+
+  // Signal managing the two-way binding demo.
+  // Note: Signals are unidirectional by default, so here we manually update
+  // the "name" signal via the input event to simulate two-way binding.
+  name = signal(''); 
+
+  updateName(event: Event) {
+    const newValue = (event.target as HTMLInputElement).value;
+    this.name.set(newValue);
+  }
 }
